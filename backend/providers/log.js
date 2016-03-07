e2180856b8c48f57ec77482dc6f2251c5f9a6518
boo1ean <boo1ean0807@gmail.com>
2016-03-07 18:50:05 +0200

    Change default log format

--- backend/providers/log.js
+++ backend/providers/log.js
@@ -1,1 +1,10 @@
-module.exports = require('winston');
+var winston = require('winston');
+module.exports = new winston.Logger({
+	transports: [
+		new winston.transports.Console({
+			handleExceptions: true,
+			json: true
+		})
+	],
+	exitOnError: false
+});
