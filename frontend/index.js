2bc21b54fb3c2e43456daa62475ef9ec87dc6fc7
boo1ean <boo1ean0807@gmail.com>
2016-03-12 18:07:43 +0200

    Update to new react-redux-router

--- frontend/index.js
+++ frontend/index.js
@@ -4,18 +4,20 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import { syncHistoryWithStore } from 'react-router-redux';
 import store from './modules/main/store';
 import routes from './modules/main/routes';
 import DevTools from './modules/main/components/dev-tools';
 
 injectTapEventPlugin();
+const history = syncHistoryWithStore(browserHistory, store);
 
 class Root extends Component {
 	render () {
 		return (
 			<Provider store={store}>
 				<div>
-					<Router history={browserHistory}>
+					<Router history={history}>
 						{routes}
 					</Router>
 					<DevTools />
